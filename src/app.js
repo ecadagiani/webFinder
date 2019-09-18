693fe7b778619b3f7bb531b184af5d7ec877c394
Eden <e.cadagiani@gmail.com>
2019-09-19 00:06:51 +0200

    setup mongo and mongoose (bug)
    add whitelist
    fix match

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const puppeteer = require('puppeteer');
 const {get} = require('lodash');
+const mongoose = require('mongoose');
 global.Promise = require('bluebird');
+
 
 const {fetchPage} = require('./lib/fetchPage');
 
 const config = require('./config');
 
+const {MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_PORT, MONGO_DATABASE} = process.env;
+const mongoUri = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`;
+console.log(mongoUri)
+mongoose.connect(mongoUri, {useNewUrlParser: true});
 
 (async () => {
     const browser = await puppeteer.launch();
--- src/lib/fetchPage.js
+++ src/lib/fetchPage.js
@@ -1,28 +1,38 @@
 const config = require('../config');
 
 async function fetchLinks(page) {
-    return await page.$$eval('a', anchors => {
+    const links = await page.$$eval('a', anchors => {
 
         function isHidden(el) {
             return (el.offsetParent === null);
         }
-// todo only get http:// or https:// links (not mailto: javascript:vois(0) ... )
+
         return anchors
         // get only visible link, because invisible link is a common trap to catch web crawler
             .filter(anchor => !isHidden(anchor) )
-            .map(anchor => anchor.href);
+            .map(anchor => anchor.href)
+            .filter(href => href.includes('http'));
     });
+
+    return links.filter(href =>
+        config.whitelist.every(pattern => {
+            const reg = new RegExp(pattern);
+            return !reg.test(href);
+        })
+    );
 }
 
 async function checkSearchSelectors(page) {
     const result = await Promise.map(config.searchSelectors, async (searchSelector) => {
         return await page.$$eval(searchSelector, el => {
-            return !!el;
+            return Array.isArray(el) && el.length > 0;
         });
     });
-
-    console.log(result); // todo always true
     return result.includes(true);
+}
+
+async function getPageLanguage(page) {
+    return null;
 }
 
 
@@ -31,6 +41,7 @@ async function fetchPage(page) {
     return {
         match: await checkSearchSelectors(page),
         links: await fetchLinks(page),
+        language: await getPageLanguage(page),
     };
 }
 
